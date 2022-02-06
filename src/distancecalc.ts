626857d7ca36c44506210ca204d29baeb6cac9ae
JanDN312 <J.Dario@gmx.de>
2022-02-07 00:47:59 +0100

    1.0.1

---
+++ src/distancecalc.ts
@@ -0,0 +1,15 @@
+export class distcalcmethods{
+    getMinkowskiDistance(point1: Array<number>, point2: Array<number>,p: number){
+        var result:number=0
+        for (let i = 0; i < point1.length; i++){
+            result=result+(((point1[i]-point2[i])**2)**0.5)**p
+        }
+        return result**(1/p)
+    }
+    getManhattanDistance(point1: Array<number>, point2: Array<number>){
+        this.getMinkowskiDistance(point1,point2,1)
+    }
+    getEuclideanDistance(point1: Array<number>, point2: Array<number>){
+        this.getMinkowskiDistance(point1,point2,2)
+    }
+}
