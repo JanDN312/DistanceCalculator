9ddd8c1fc36467c86e97799fbba9535613bf91a9
JanDN312 <J.Dario@gmx.de>
2022-02-07 13:43:34 +0100

    publicupdate

--- test.ts
+++ test.ts
@@ -2,6 +2,6 @@ import {Distcalcmethods} from "https://deno.land/x/distance_calculator@2.0.0/mod
 
 var testdata:Array<number[]>=[[1,2,3],[3,6,2]]
 const dist=new Distcalcmethods()
-console.log(dist.getMinkowskiDistance(testdata[0],testdata[1],5))
+console.log(dist.getMinkowskiDistance(testdata[0],testdata[1],5))//point1, point2, p-Minkowski
 console.log(dist.getManhattanDistance(testdata[0],testdata[1]))
 console.log(dist.getEuclideanDistance(testdata[0],testdata[1]))
